db3e74bde2662a8c52b1377b9b9236fd8a86fd12
mateusz7 <mateuszkicinski@onet.pl>
2020-04-06 15:05:04 +0200

    edycja pliku models.py

--- oferty/models.py
+++ oferty/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Oferta(models.Model):
+    tytul = models.CharField(max_length=80)
+    tresc = models.TextField()
+    data = models.DateTimeField(default=timezone.now)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name_plural = "Oferty"
+
+    def __str__(self):
+        return self.tytul + '_' + self.autor.username
