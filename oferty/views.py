86caf7141a3912211a16697d6846a7009f614e31
pwojton <piotr.wojton.01@gmail.com>
2020-04-06 14:53:54 +0200

    kończenie dodawania szablonu

--- oferty/views.py
+++ oferty/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+oferty = [
+    {
+        'autor': 'ZbrSnd',
+        'tytul': 'Pomoc z matmy',
+        'tresc': 'Treść',
+        'data': 'Marzec 20, 2020',
+    },
+    {
+        'autor': 'Pwojton',
+        'tytul': 'Pomoc z matmy',
+        'tresc': 'Treść',
+        'data': 'Marzec 20, 2020',}
+
+]
 
 def glowna(request):
-    return render(request, 'oferty/strona_glowna.html')
+    context = {
+        'oferty': oferty
+    }
+
+    return render(request, 'oferty/strona_glowna.html', context)
