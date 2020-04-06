8b044cb81ba77d51cef026edd9f5a63a15fadb92
zbrsnd <zbrsnd@gmail.com>
2020-04-06 14:49:34 +0200

    rozpoczęcie dodawania szablonu

--- new_app/settings.py
+++ new_app/settings.py
@@ -100,9 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pl-PL'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
@@ -115,3 +115,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'oferty/static') 
