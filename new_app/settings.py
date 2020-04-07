c0ee76e3235f84cecf010563cb1dda8153721b79
mateusz7 <mateuszkicinski@onet.pl>
2020-04-07 11:23:07 +0200

    edycja plików w katalogu new_app

--- new_app/settings.py
+++ new_app/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'oferty/static') 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'glowna'
