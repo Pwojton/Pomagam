d54113fa193275874e3ffca007f9b18134bed52c
zbrsnd <zbrsnd@gmail.com>
2020-04-08 12:22:23 +0200

    edycja podstawowego szablonu i pliku urls.py

--- oferty/urls.py
+++ oferty/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
+from .views import OfertaDetailView, OfertaCreateView, OfertaUpdateView, OfertaDeleteView,\
+                   KomentarzCreateView, KomentarzUpdateView, KomentarzDeleteView
 
 urlpatterns = [
     path('', views.glowna, name='glowna'),
+    path('oferta/<int:pk>', OfertaDetailView.as_view(), name='oferta_szczegoly'),
+    path('oferta/nowa', OfertaCreateView.as_view(), name='oferta_utworz'),
+    path('oferta/<int:pk>/aktualizuj', OfertaUpdateView.as_view(), name='oferta_aktualizuj'),
+    path('oferta/<int:pk>/usun', OfertaDeleteView.as_view(), name='oferta_usun'),
+    path('post/<int:pk>/komantarz', KomentarzCreateView.as_view(), name='komentarz_utworz'),
+    path('post/<int:pk>/komantarz/aktualizuj', KomentarzUpdateView.as_view(), name='komentarz_aktualizuj'),
+    path('post/<int:pk>/komantarz/usun', KomentarzDeleteView.as_view(), name='komentarz_usun'),
 ]
